847a0a5cc08da1820d63a376ba4d42b6635da4a1
Shourya <65444247+ShouryaSaklecha@users.noreply.github.com>
2024-09-29 21:17:28 +1000

    Back to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage'; // Adjusted import
 import EducationPage from './pages/EducationPage';
@@ -12,14 +12,14 @@ const App = () => {
     <Router>
       <Header />
       <div className="p-4">
-        <Switch>
-          <Route exact path="/" element={<HomePage />} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/education" element={<EducationPage />} />
           <Route path="/work" element={<WorkPage />} />
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/contact" element={<ContactPage />} />
           {/* Define other routes here */}
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
