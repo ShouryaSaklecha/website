f31b0ff5488acf4d180c87397f00dfe805086de9
Shourya <65444247+ShouryaSaklecha@users.noreply.github.com>
2025-12-31 22:38:32 +0530

    formatting

--- src/pages/BlogMoment2025.jsx
+++ src/pages/BlogMoment2025.jsx
@@ -4,44 +4,44 @@ const BlogMoment2025 = () => {
   return (
     <div className="bg-black text-[#FDFFFF] p-8 min-h-screen">
       <div className="max-w-3xl mx-auto">
-        <h1 className="font-stardom text-4xl text-[#FF9F1C] mb-6">A moment for 2025</h1>
+        <h1 className="font-stardom text-4xl text-[#FF9F1C] mb-6">A Moment for 2025</h1>
         <p className="mb-6">
           I am not usually publicly retrospective; let’s start there. But I promised myself to write more this year, which is why this is probably my last opportunity to put something out there. I have 4 hours before this year ends, so please forgive my content being a year recap :p
         </p>
 
-        <h2 className="text-2xl font-semibold mb-2">One for Work</h2>
+        <h2 className="text-2xl font-semibold mb-2">A Moment for Work</h2>
         <p className="mb-6">
           2025 was my first year as a full-time employee. Cuvos has been a truly rewarding experience, and I am incredibly glad for the opportunities that I got at work. I worked closely on our Hawkeye technology for the first half of 2025, using really interesting architectures and ideas with High-Level Synthesis tools to manage AXI protocols. I also did some cool C++ implementations on the Raspberry Pi, which was a lot of fun.
           <br />
           My one moment at work was how well I was supported by the team when my grandma passed. To have them allow me to leave a workday at a text’s notice meant the world. More on this later.
         </p>
 
-        <h2 className="text-2xl font-semibold mb-2">A moment in travel</h2>
+        <h2 className="text-2xl font-semibold mb-2">A Moment for travel</h2>
         <p className="mb-6">
           I’ve moved around the most in any year this year. Apart from 2 trips to India, I got to see Vietnam and New Zealand, while also travelling a bit in Australia with my parents, who had a vacation after 12 years.
           <br />
           My one moment in travel has to be hiking at Mt. Cook. Imagine snow mountains on all sides, no visibility, strong winds that would’ve blown a kid away, and rain on top of it all. Now imagine making that walk with two mates, listening (or at least trying to) to music, and THAT moment when the funny conversation turns into something deep. Cliche, but truly THE moment. In that moment, I left the grief in my life behind.
         </p>
 
-        <h2 className="text-2xl font-semibold mb-2">A Moment in Fitness</h2>
+        <h2 className="text-2xl font-semibold mb-2">A Moment for Fitness</h2>
         <p className="mb-6">
           I have probably been the unhealthiest I have ever been in 2025. Between sitting at a desk for 8 hours a day and just travelling, discovering Sydney, I gained 10 kgs (yikes). It was the hardest I have had to work on my body this year, but I am glad I got my act in order before the year finished.
           <br />
           I haven’t cycled in a while. My cardiovascular strength is probably at its worst point. However, I had one moment of true joy when I finished a 10k on the bike right after a 40-minute strength workout. It’s not much by any means, but I am glad I at least showed up for myself.
         </p>
 
-        <h2 className="text-2xl font-semibold mb-2">A Moment In Learning</h2>
+        <h2 className="text-2xl font-semibold mb-2">A Moment for Learning</h2>
         <p className="mb-6">
           I did quite a few new things this year. I set up my first ever server, which taught me some Linux stuff, took a course in ASIC design, learnt heaps through Spot Tutors. And probably that is where my one moment in learning was.
           <br />
           It was really difficult to let go of our first ever employee. The entire journey of hiring someone, taking care of them, being responsible for their livelihood, taught me an insane amount. I hope I get to do this again (the hiring part, not the firing one).
         </p>
 
-        <h2 className="text-2xl font-semibold mb-2">A Moment in Life</h2>
+        <h2 className="text-2xl font-semibold mb-2">A Moment for Life</h2>
         <p className="mb-6">
           By a lot of measures, I spent the year just living easy. I celebrated my graduation with my parents, took them around Australia; saw AC/DC, Metallica/Oasis/Green Day, La Bohème at the Opera House; travelled; enjoyed my rent.
           <br />
-          But my one moment in life this year was the day before losing my grandma. My dad called me Wednesday afternoon while I was at work, saying grandma really wants to see me. I don’t know what got into me, and I told our CEO that I need to leave for India as soon as she can let me. She let me leave immediately, no questions asked about how long, where, when, anything. I was on a plane in 4 hours. I reached India the following night after a 12-hour layover in Delhi. My grandma was in the ICU, awake, looking at me. I spoke for 4 hours while she listened. I lost her the next afternoon.
+          But my one moment in life this year was the day <i>before</i> I lost my grandma. My dad called me Wednesday afternoon while I was at work, saying grandma really wants to see me. I don’t know what got into me, and I told our CEO that I need to leave for India as soon as she can let me. She let me leave immediately, no questions asked about how long, where, when, anything. I was on a plane in 4 hours. I reached India the following night after a 12-hour layover in Delhi. My grandma was in the ICU, awake, looking at me. I spoke for 4 hours while she listened. I lost her the next afternoon.
         </p>
 
         <p className="mb-6">
